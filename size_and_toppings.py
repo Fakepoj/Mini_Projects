86b9006b73fc5beafde2ebe52a38cc21abced428
Kenneth Fatore <fatorekenneth@gmail.com>
2026-09-01 14:40:48 +0100

    Update size_and_toppings.py

--- size_and_toppings.py
+++ size_and_toppings.py
@@ -1,9 +1,9 @@
 def make_coffee(size, *toppings):
-    """Prepares a coffee  an arbitrary list of toppings."""
+    """Prepares a coffee an arbitrary list of toppings."""
     print(f"Making a {size} coffee with the following toppings:")
 
     if not toppings:
-        print("- Plain (Nowith toppings)")
+        print("- Plain (No toppings)")
     else:
         for topping in toppings:
             print(f"- {topping}")
