206dfe45bc79b8ea8cf2709bd9166098dcd4faee
Kenneth <fatorekenneth@gmail.com>
2026-09-01 14:25:38 +0100

    id_input_validation_with_exception

--- Input_Validation_with_Exception_Handling.py
+++ Input_Validation_with_Exception_Handling.py
@@ -4,5 +4,5 @@ try:
     user_id = int(user_input)
     print('Access Granted!')
 except ValueError:
-    print(f"Sorry, '{user_input}' is not a valid whole number.")
+    print(f"Sorry, '{user_input}' is not a valid whole number!")
     user_id = None
